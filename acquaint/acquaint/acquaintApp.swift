067e43229ff38371a1e675eeff5fb1b0e613d8f6
Erica Wo <wo_erica@berkeley.edu>
2021-05-05 22:23:06 -0700

    fixing stylesheet

---
+++ acquaint/acquaint/acquaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  acquaintApp.swift
+//  acquaint
+//
+//  Created by Erica Wo on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct acquaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ skin-u/skin-u/skin_uApp.swift
@@ -0,0 +1,17 @@
+//
+//  skin_uApp.swift
+//  skin-u
+//
+//  Created by Erica Wo on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct skin_uApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
